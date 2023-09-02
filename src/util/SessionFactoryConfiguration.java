7dc30b4a904bfc9e0b57f54ade37f3a5ebf6b969
Thasuni Induma <thasuniinduma@gmail.com>
2023-09-01 16:22:53 -0800

    first commit

---
+++ src/util/SessionFactoryConfiguration.java
@@ -0,0 +1,39 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import entity.BookEntity;
+import entity.LaptopEntity;
+import entity.StudentEntity;
+import entity.SubjectEntity;
+
+
+
+public class SessionFactoryConfiguration {
+
+    private static SessionFactoryConfiguration sessionFactoryConfiguration;
+
+    private SessionFactory sessionFactory;
+
+    private SessionFactoryConfiguration() {
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(LaptopEntity.class)
+        .addAnnotatedClass(StudentEntity.class)
+        .addAnnotatedClass(BookEntity.class)
+        .addAnnotatedClass(SubjectEntity.class);
+        
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static SessionFactoryConfiguration getInstance() {
+        return sessionFactoryConfiguration == null ? sessionFactoryConfiguration = new SessionFactoryConfiguration()
+                : sessionFactoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
